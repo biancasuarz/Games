638d2d0a07bbb43796ec8e2c4e3eb9a227e1ddb4
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2024-11-02 19:08:16 -0300

    Jogo simples de corrida usando Javax Swing

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    private int position;
+
+    public Car() {
+        this.position = 0;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        position += (int) (Math.random() * 5);
+    }
+}
+
---
+++ src/Game.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Corrida de Carros");
+        CarRace race = new CarRace();
+
+        frame.add(race);
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
